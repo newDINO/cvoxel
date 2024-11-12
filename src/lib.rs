fca2bbb9575a49a81a84a2b3cb923321a1aa27e4
lluvz <2572502109@qq.com>
2024-11-12 11:43:21 +0800

    maxfix

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,39 @@
-use nalgebra::{SimdPartialOrd, Vector3};
+use nalgebra::Point3;
 
 pub struct Triangle {
-    pub vertex0: Vector3<f32>,
-    pub vertex1: Vector3<f32>,
-    pub vertex2: Vector3<f32>,
+    pub vertex0: Point3<f32>,
+    pub vertex1: Point3<f32>,
+    pub vertex2: Point3<f32>,
 }
+
 impl Triangle {
     /// ```
-    /// use nalgebra::vector;
+    /// use nalgebra::point;
     /// use cvoxel::{Aabb, Triangle};
     /// let triangle = Triangle {
-    ///     vertex0: vector![0.0, -3.0, 2.0],
-    ///     vertex1: vector![-1.0, 2.0, 1.0],
-    ///     vertex2: vector![1.0, -1.0, 3.0],
+    ///     vertex0: point![0.0, -3.0, 2.0],
+    ///     vertex1: point![-1.0, 2.0, 1.0],
+    ///     vertex2: point![1.0, -1.0, 3.0],
     /// };
     /// let aabb = triangle.aabb();
-    /// assert_eq!(aabb, Aabb::new(vector![-1.0, -3.0, 1.0], vector![1.0, 2.0, 3.0]));
+    /// assert_eq!(aabb, Aabb::new(point![-1.0, -3.0, 1.0], point![1.0, 2.0, 3.0]));
     /// ```
     pub fn aabb(&self) -> Aabb {
-        let min = self.vertex0.simd_min(self.vertex1).simd_min(self.vertex2);
-        let max = self.vertex0.simd_max(self.vertex1).simd_max(self.vertex2);
-        Aabb {
-            max, min
-        }
+        let min = self.vertex0.inf(&self.vertex1).inf(&self.vertex2);
+        let max = self.vertex0.sup(&self.vertex1).sup(&self.vertex2);
+        Aabb { max, min }
     }
 }
 
 #[derive(PartialEq, Debug)]
 pub struct Aabb {
-    pub min: Vector3<f32>,
-    pub max: Vector3<f32>,
+    pub min: Point3<f32>,
+    pub max: Point3<f32>,
 }
 
 impl Aabb {
-    pub fn new(min: Vector3<f32>, max: Vector3<f32>) -> Self {
-        Self {
-            min, max
-        }
+    pub fn new(min: Point3<f32>, max: Point3<f32>) -> Self {
+        Self { min, max }
     }
 }
 
